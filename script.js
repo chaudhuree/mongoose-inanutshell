8fb73dd1be31449acff223a755dff146633d17d4
chaudhuree <chaudhuree00@gmail.com>
2022-12-09 13:29:19 +0600

    docs: find with where method

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const User=require('./Users')
 const createUser=async () =>{
   try {
     const user= new User({
-      name:'sohan',
-      age:27,
-      email:'Chaudhuree00@gmail.com',
+      name:'kabir',
+      age:28,
+      email:'myemail@gmail.com',
       createdAt:new Date(),
       updatedAt:new Date(),
       hobbies:['video games','badminton'],
@@ -25,3 +25,17 @@ const createUser=async () =>{
   }
 }
 createUser();
+
+
+////////////////////////
+//      find user
+////////////////////////
+const findUserById = async (userId) =>{
+  const findedUser=await User.findById(userId);
+  const anotherFind= await User.where("name").equals('sohan').select('age')
+
+  console.log("▶ ➡ file: script.js:31 ➡ findUsers ➡ findedUser", findedUser);
+  console.log("▶ ➡ file: script.js:36 ➡ findUserById ➡ anotherFind", anotherFind);
+  
+}
+findUserById("6392dcb3ac2603bced10b5c2")
