d8d16937b1335584bb46e4e4777ee89cb8e5181c
chaudhuree <chaudhuree00@gmail.com>
2022-12-09 12:26:42 +0600

    docs: some additional edit

--- Users.js
+++ Users.js
@@ -1,17 +1,31 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const addressSchema=mongoose.Schema({
-  street:String,
-  road:Number
+const addressSchema = mongoose.Schema({
+  street: {
+    type: String,
+    lowercase: true
+  },
+  road: Number
 })
-const userSchema= mongoose.Schema({
-  name:String,
-  age:Number,
-  email:String,
-  createdAt:Date,
-  updatedAt:Date,
-  hobbies:[String],
-  address:addressSchema
+const userSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  email: {
+    type: String,
+    lowercase: true,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date(),
+    immutable: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: () => new Date()
+  },
+  hobbies: [String],
+  address: addressSchema
 })
 
-module.exports=mongoose.model("UserData",userSchema)
+module.exports = mongoose.model("UserData", userSchema)
